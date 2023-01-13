6d7a70a9f27d410aeb55a2550d209fde537a829c
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-01-13 13:57:10 +0800

    Update boc.py

--- boc.py
+++ boc.py
@@ -39,7 +39,8 @@ receive_list=['782568799@qq.com']
 
 def sendmail(receive_mail,title=None):
     send_usr = '782568799@qq.com'  # 发件人
-    send_pwd ='pdnfrwfijoewbeff'#'BUQRRQFPUANBCWMY' # 授权码，邮箱设置
+    send_pwd=os.environ["COOKIE1"]
+#     send_pwd =
     receive = receive_mail#'782568799@qq.com'  # 接收者
     content = '发送于{}<p><a href="{}">GBPCNY-中国银行现价动态,from github</a></p>'.format(
         str(datetime.datetime.now(pytz.timezone('Asia/Chongqing'))).replace(":",".")[:16],url)
