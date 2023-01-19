5ffcf1adfeefc28574990f9eef283e755bfc0fda
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-01-19 15:10:19 +0800

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -49,11 +49,10 @@ def print_folder_tree(path, depth=0):
             files.append(i_path)
     return files 
 
-
+os.mkdir('data')
 df=pd.DataFrame({"time":time.ctime(),"zone":"GMT","currency":"GBP英镑"},index=['test'])
-df.to_csv('df.csv',encoding=os.environ["ENCODE"])
-print(os.path.abspath(''))
-print(print_folder_tree(os.path.abspath('')))
+df.to_csv('data/df.csv',encoding=os.environ["ENCODE"])
+
 
 
 
