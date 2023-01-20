5af4fc3942e3aaf54dc8ce63072986443c160614
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-01-20 16:19:49 +0800

    Update send2.py

--- send2.py
+++ send2.py
@@ -2,5 +2,8 @@
 file='data/df.csv'
 import os
 print(4,os.path.abspath(''))
-for a,b,c in os.walk(os.path.abspath('')):
-  print('------',a,b,c)
+
+import pandas as pd
+df=pd.read_csv(file)
+print(8,type(df),len(df))
+print(9,df)
