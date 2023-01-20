f64cf0b702977c7db85179515f1762ef15b367b5
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-01-21 00:50:18 +0800

    Update send2.py

--- send2.py
+++ send2.py
@@ -1,8 +1,8 @@
 ## -*- coding:utf-8 -*-
-file='data/df.csv'
+
 import os
 print(4,os.path.abspath(''))
-
+file=os.environ["SDF"]
 import pandas as pd
 df=pd.read_csv(file)
 print(8,type(df),len(df))
