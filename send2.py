9465e5f24748160197fefc34e686c4dd5fee17b2
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-01-21 01:10:26 +0800

    Update send2.py

--- send2.py
+++ send2.py
@@ -2,8 +2,11 @@
 
 import os
 print(4,os.path.abspath(''))
-file=os.environ["SDF"]
-import pandas as pd
-df=pd.read_csv(file)
-print(8,type(df),len(df))
-print(9,df)
+for a,b,c in os.walk(os.path.abspath('')):
+  print(a,b,c,'\n\n')
+  
+# file=os.environ["SDF"]
+# import pandas as pd
+# df=pd.read_csv(file)
+# print(8,type(df),len(df))
+# print(9,df)
